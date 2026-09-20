0b41200ed82c899dbd1d4e14029ad56a939c9911
Mohamed Saad <111496655+mo7medSa3d@users.noreply.github.com>
2026-09-20 12:24:16 +0300

    chore: remove obsolete agent freshness import

--- src/lib/print-job-service.ts
+++ src/lib/print-job-service.ts
@@ -7,7 +7,7 @@ import { and, eq, sql } from "drizzle-orm";
 import { nanoid } from "./nanoid";
 import { canonicalize } from "./canonicalize";
 import { MAX_AGENT_IN_FLIGHT_JOBS } from "./job-delivery";
-import { agentStaleThresholdSeconds } from "./agent-availability";
+
 import { enforceTenantJobEntitlements } from "./entitlements";
 import { logInfo } from "./log";
 
