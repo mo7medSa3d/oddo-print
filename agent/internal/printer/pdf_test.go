e6b518de7ec585e1566a7dc097bf005411a21d8f
Mohamed Saad <111496655+mo7medSa3d@users.noreply.github.com>
2026-09-25 00:32:52 +0300

    refactor: remove unused rotatedPDF test helper

--- agent/internal/printer/pdf_test.go
+++ agent/internal/printer/pdf_test.go
@@ -33,22 +33,6 @@ func validPDF() []byte {
 		"trailer\n<< /Size 5 /Root 1 0 R >>\n" +
 		"startxref\n268\n%%EOF\n")
 }
-func rotatedPDF() []byte {
-	// Same minimal PDF with a valid /Rotate 90 page entry and corrected xref offsets.
-	return []byte("%PDF-1.4\n" +
-		"1 0 obj\n<< /Type /Catalog /Pages 2 0 R >>\nendobj\n" +
-		"2 0 obj\n<< /Type /Pages /Kids [3 0 R] /Count 1 >>\nendobj\n" +
-		"3 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [0 0 595 842] /Rotate 90 /Resources << >> /Contents 4 0 R >>\nendobj\n" +
-		"4 0 obj\n<< /Length 0 >>\nstream\n\nendstream\nendobj\n" +
-		"xref\n0 5\n0000000000 65535 f \n" +
-		"0000000009 00000 n \n" +
-		"0000000058 00000 n \n" +
-		"0000000115 00000 n \n" +
-		"0000000230 00000 n \n" +
-		"trailer\n<< /Size 5 /Root 1 0 R >>\n" +
-		"startxref\n279\n%%EOF\n")
-}
-
 // --- Test 7: valid PDF payload passes validation -------------------------
 
 func TestValidPDFPassesValidation(t *testing.T) {
